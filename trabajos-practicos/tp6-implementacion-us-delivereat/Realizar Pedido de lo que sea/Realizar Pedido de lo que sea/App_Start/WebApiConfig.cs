4f326108ae958062ad533dfdf9fd9a61e7176b63
zArielz <ariel.strasorier@gmail.com>
2019-09-21 14:08:30 -0300

    Creacion del proyecto , esta planteado el html

---
+++ trabajos-practicos/tp6-implementacion-us-delivereat/Realizar Pedido de lo que sea/Realizar Pedido de lo que sea/App_Start/WebApiConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace Realizar_Pedido_de_lo_que_sea
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Configuración y servicios de API web
+
+            // Rutas de API web
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ trabajos-practicos/tp6-implementacion-us-delivereat/Realizar Pedido de lo que sea/Realizar Pedido de lo que sea/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Realizar_Pedido_de_lo_que_sea.WebApiApplication" Language="C#" %>
