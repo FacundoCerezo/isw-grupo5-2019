246e29c61d10096a1bfd7c74a2f7b0a459272f65
francobosio <bosiofranco94@gmail.com>
2019-09-22 02:52:44 -0300

    agrego funcionalidad basica

---
+++ trabajos-practicos/tp6-implementacion-us-delivereat/Realizar Pedido de lo que sea/Realizar Pedido de lo que sea/Scripts/deliverEat.js
@@ -0,0 +1,9 @@
+﻿var deliverEatApp = angular.module("deliverEatApp", []);
+deliverEatApp.controller("controller", function ($scope) {
+
+    $scope.tarjeta = false;
+    $scope.horario = false;
+    $scope.ciudades = [{ id: 1, nombre: "Cordoba" }, { id: 2, nombre: "Rosario" }, { id: 3, nombre: "Buenos Aires" }];
+
+
+});
