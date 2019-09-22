69160cf2a2a1e3c8d0f2e4f15795ffc9226b6adb
francobosio <bosiofranco94@gmail.com>
2019-09-22 17:57:32 -0300

    Agrego validaciones al html, programo funcion limpiar

--- trabajos-practicos/tp6-implementacion-us-delivereat/Realizar Pedido de lo que sea/Realizar Pedido de lo que sea/Scripts/deliverEat.js
+++ trabajos-practicos/tp6-implementacion-us-delivereat/Realizar Pedido de lo que sea/Realizar Pedido de lo que sea/Scripts/deliverEat.js
@@ -1,9 +1,30 @@
 ﻿var deliverEatApp = angular.module("deliverEatApp", []);
 deliverEatApp.controller("controller", function ($scope) {
 
-    $scope.tarjeta = false;
-    $scope.horario = false;
-    $scope.ciudades = [{ id: 1, nombre: "Cordoba" }, { id: 2, nombre: "Rosario" }, { id: 3, nombre: "Buenos Aires" }];
+    $scope.tarjetaFlag = false;
+    $scope.horarioFlag = false;
+    $scope.ciudades = [{ id: 1, nombre: "Cordoba" },
+        { id: 2, nombre: "Rosario" },
+        { id: 3, nombre: "Buenos Aires" }];
+    $scope.ciudadComercio = $scope.ciudades[0];
+    $scope.ciudadEntrega = $scope.ciudades[0];
 
-
+    $scope.limpiarFormulario = function () {
+        $scope.calleComercio = "";
+        $scope.numeroComercio = "";
+        $scope.ciudadComercio = $scope.ciudades[0];
+        $scope.descripcionComercio = "";
+        $scope.calleEntrega = "";
+        $scope.numeroEntrega = "";
+        $scope.ciudadEntrega = $scope.ciudades[0];
+        $scope.descripcionEntrega = "";
+        $scope.monto = "";
+        $scope.numeroTarjeta = "";
+        $scope.apellidoNombre = "";
+        $scope.anioTarjeta = "";
+        $scope.mesTarjeta = "";
+        $scope.cvcTarjeta = "";
+        $scope.horarioEntrega = "";
+        $scope.frmPedido.$setPristine(true);
+    }
 });
