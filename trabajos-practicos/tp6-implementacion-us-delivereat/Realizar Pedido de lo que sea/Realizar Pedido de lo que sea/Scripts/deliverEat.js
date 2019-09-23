1d5eb24a66377c7e77ca5e4969ce46b33c62518c
francobosio <bosiofranco94@gmail.com>
2019-09-23 03:49:13 -0300

    agrego metodo post, modifico objeto pedido en js

--- trabajos-practicos/tp6-implementacion-us-delivereat/Realizar Pedido de lo que sea/Realizar Pedido de lo que sea/Scripts/deliverEat.js
+++ trabajos-practicos/tp6-implementacion-us-delivereat/Realizar Pedido de lo que sea/Realizar Pedido de lo que sea/Scripts/deliverEat.js
@@ -1,30 +1,66 @@
 ﻿var deliverEatApp = angular.module("deliverEatApp", []);
-deliverEatApp.controller("controller", function ($scope) {
+deliverEatApp.controller("controller", function ($scope, $http) {
 
     $scope.tarjetaFlag = false;
     $scope.horarioFlag = false;
     $scope.ciudades = [{ id: 1, nombre: "Cordoba" },
         { id: 2, nombre: "Rosario" },
         { id: 3, nombre: "Buenos Aires" }];
-    $scope.ciudadComercio = $scope.ciudades[0];
-    $scope.ciudadEntrega = $scope.ciudades[0];
 
     $scope.limpiarFormulario = function () {
-        $scope.calleComercio = "";
-        $scope.numeroComercio = "";
-        $scope.ciudadComercio = $scope.ciudades[0];
-        $scope.descripcionComercio = "";
-        $scope.calleEntrega = "";
-        $scope.numeroEntrega = "";
-        $scope.ciudadEntrega = $scope.ciudades[0];
-        $scope.descripcionEntrega = "";
-        $scope.monto = "";
-        $scope.numeroTarjeta = "";
-        $scope.apellidoNombre = "";
-        $scope.anioTarjeta = "";
-        $scope.mesTarjeta = "";
-        $scope.cvcTarjeta = "";
-        $scope.horarioEntrega = "";
+        $scope.Pedido = {
+            Monto: "",
+            FechaHoraEntrega: "",
+            Imagen: "",
+            Descripcion: "",
+            FormaPagoId: 1,
+            EstadoPedidoId: 1,
+
+            DomicilioOrigen: {
+                Calle: "",
+                Numero: "",
+                Descripcion: "",
+                Ciudad: $scope.ciudades[0]
+            },
+
+            DomicilioDestino: {
+                Calle: "",
+                Numero: "",
+                Descripcion: "",
+                Ciudad: $scope.ciudades[0]
+            },
+
+            Tarjeta: {
+                Numero: "",
+                NombreTitular: "",
+                Anio: "",
+                Mes: "",
+                CVC: ""
+            },
+        }
         $scope.frmPedido.$setPristine(true);
+    };
+
+    $scope.grabar = function () {
+        $scope.Pedido.FormaPagoId = $scope.tarjetaFlag ? 2 : 1;
+
+        if ($scope.tarjetaFlag) {
+            $scope.Pedido.Monto = "";
+        } else {
+            $scope.Pedido.Tarjeta.Numero = "";
+            $scope.Pedido.Tarjeta.NombreTitular = "";
+            $scope.Pedido.Tarjeta.Anio = "";
+            $scope.Pedido.Tarjeta.Mes = "";
+            $scope.Pedido.Tarjeta.CVC = "";
+        };
+        if (!$scope.horarioFlag) {
+            $scope.Pedido.FechaHoraEntrega = "";
+        };
+
+        $http.post("api/pedidos", $scope.Pedido).then(function (response) {
+            window.alert("Pedido Enviado");
+        })
     }
+
+
 });
