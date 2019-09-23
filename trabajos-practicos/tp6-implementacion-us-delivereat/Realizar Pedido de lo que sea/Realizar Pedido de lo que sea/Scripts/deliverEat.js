353feb8adc7c2af8be0c30300345690c1380e826
francobosio <bosiofranco94@gmail.com>
2019-09-23 15:32:43 -0300

    el sistema ya permite cargar pedidos, con y sin tarjeta, con y sin horario

--- trabajos-practicos/tp6-implementacion-us-delivereat/Realizar Pedido de lo que sea/Realizar Pedido de lo que sea/Scripts/deliverEat.js
+++ trabajos-practicos/tp6-implementacion-us-delivereat/Realizar Pedido de lo que sea/Realizar Pedido de lo que sea/Scripts/deliverEat.js
@@ -7,10 +7,40 @@ deliverEatApp.controller("controller", function ($scope, $http) {
         { id: 2, nombre: "Rosario" },
         { id: 3, nombre: "Buenos Aires" }];
 
+    $scope.Pedido = {
+        DomicilioOrigen: {
+            Calle: "",
+            Numero: "",
+            Descripcion: "",
+            Ciudad: $scope.ciudades[0],
+            CiudadId: 0
+        },
+        DomicilioDestino: {
+            Calle: "",
+            Numero: "",
+            Descripcion: "",
+            Ciudad: $scope.ciudades[0],
+            CiudadId: 0
+        },
+        Monto: "",
+        EstadoPedidoId: 1,
+        FormaPagoId: 1,
+        Tarjeta: {
+            Numero: "",
+            NombreTitular: "",
+            Anio: "",
+            Mes: "",
+            CVC: ""
+        },
+        FechaHoraEntrega: null,
+        Imagen: "",
+        Descripcion: "",
+    }
+
     $scope.limpiarFormulario = function () {
         $scope.Pedido = {
             Monto: "",
-            FechaHoraEntrega: "",
+            FechaHoraEntrega: null,
             Imagen: "",
             Descripcion: "",
             FormaPagoId: 1,
@@ -20,14 +50,14 @@ deliverEatApp.controller("controller", function ($scope, $http) {
                 Calle: "",
                 Numero: "",
                 Descripcion: "",
-                Ciudad: $scope.ciudades[0]
+                Ciudad: $scope.ciudades[0],
             },
 
             DomicilioDestino: {
                 Calle: "",
                 Numero: "",
                 Descripcion: "",
-                Ciudad: $scope.ciudades[0]
+                Ciudad: $scope.ciudades[0],
             },
 
             Tarjeta: {
@@ -45,21 +75,39 @@ deliverEatApp.controller("controller", function ($scope, $http) {
         $scope.Pedido.FormaPagoId = $scope.tarjetaFlag ? 2 : 1;
 
         if ($scope.tarjetaFlag) {
-            $scope.Pedido.Monto = "";
+            $scope.Pedido.Monto = 0;
         } else {
             $scope.Pedido.Tarjeta.Numero = "";
             $scope.Pedido.Tarjeta.NombreTitular = "";
             $scope.Pedido.Tarjeta.Anio = "";
             $scope.Pedido.Tarjeta.Mes = "";
-            $scope.Pedido.Tarjeta.CVC = "";
+            $scope.Pedido.Tarjeta.CVC = 0;
         };
         if (!$scope.horarioFlag) {
-            $scope.Pedido.FechaHoraEntrega = null;
+            $scope.Pedido.FechaHoraEntrega = new Date;
+        };
+
+        for (var i = 0; i < $scope.ciudades.length; i++) {
+            if ($scope.ciudades[i] === $scope.Pedido.DomicilioDestino.Ciudad) {
+                $scope.Pedido.DomicilioDestino.CiudadId = i;
+            };
+        };
+
+        for (var i = 0; i < $scope.ciudades.length; i++) {
+            if ($scope.ciudades[i] === $scope.Pedido.DomicilioOrigen.Ciudad) {
+                $scope.Pedido.DomicilioOrigen.CiudadId = i;
+            };
         };
 
         $http.post("api/pedidos", $scope.Pedido).then(function (response) {
             window.alert("Pedido Enviado");
         });
         $scope.limpiarFormulario();
+    }
+
+    $scope.grabarImagen = function() {
+        $http.post("api/uploads", $scope.FuenteImagen).then(function (response) {
+            $scope.Pedido.Imagen = response;
+        })
     }
 });
