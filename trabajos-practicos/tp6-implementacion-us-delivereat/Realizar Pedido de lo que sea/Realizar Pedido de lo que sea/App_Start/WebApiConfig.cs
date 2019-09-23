e58d43829f91949d575789cf4a819f7aabc4c52b
Facundo Cerezo <facundomcerezo@gmail.com>
2019-09-23 01:00:48 -0300

    Configurar WebApi para conectar con back

--- trabajos-practicos/tp6-implementacion-us-delivereat/Realizar Pedido de lo que sea/Realizar Pedido de lo que sea/App_Start/WebApiConfig.cs
+++ trabajos-practicos/tp6-implementacion-us-delivereat/Realizar Pedido de lo que sea/Realizar Pedido de lo que sea/App_Start/WebApiConfig.cs
@@ -5,20 +5,32 @@ using System.Web.Http;
 
 namespace Realizar_Pedido_de_lo_que_sea
 {
-    public static class WebApiConfig
-    {
-        public static void Register(HttpConfiguration config)
-        {
-            // Configuración y servicios de API web
+	public static class WebApiConfig
+	{
+		public static void Register(HttpConfiguration config)
+		{
+			// Configuración y servicios de API web
 
-            // Rutas de API web
-            config.MapHttpAttributeRoutes();
+			// Forzar salida JSON para los servicios REST
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
-        }
-    }
+			GlobalConfiguration.Configuration.Formatters.JsonFormatter.MediaTypeMappings
+			.Add(new System.Net.Http.Formatting.RequestHeaderMapping("Accept",
+				   "text/html",
+				   StringComparison.InvariantCultureIgnoreCase,
+				   true,
+				   "application/json"));
+
+			var settings =
+GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings;
+
+			// Rutas de API web
+			config.MapHttpAttributeRoutes();
+
+			config.Routes.MapHttpRoute(
+				name: "DefaultApi",
+				routeTemplate: "api/{controller}/{id}",
+				defaults: new { id = RouteParameter.Optional }
+			);
+		}
+	}
 }
